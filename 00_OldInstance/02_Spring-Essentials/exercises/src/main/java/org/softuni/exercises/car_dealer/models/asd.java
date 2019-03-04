69417ed6992446d2fc3287473db09d5a7c2b641f
ivaylo <ivanov_ir@abv.bg>
2019-03-04 16:09:35 +0200

    delete

--- 00_OldInstance/02_Spring-Essentials/exercises/src/main/java/org/softuni/exercises/car_dealer/models/asd.java
+++
@@ -1,4 +0,0 @@
-package org.softuni.exercises.car_dealer.models;
-
-public class asd {
-}
--- 01_Spring_Boot_Introduction/exercises/src/main/java/org/softuni/exercises/util/HtmlRider.java
+++
@@ -1,7 +0,0 @@
-package org.softuni.exercises.util;
-
-import java.io.IOException;
-
-public interface HtmlRider {
-    String readHtmlFile(String path) throws IOException;
-}
